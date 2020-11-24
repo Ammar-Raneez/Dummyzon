59ddd15898079dc4b940ad6a8610ae4676c460b0
Ammar <--add>
2020-11-25 00:24:21 +0530

    Migrated database

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB208XtCaQeVDvL66yA0PS-vnYLVuehO2g",
-    authDomain: "dummyzon-c93d1.firebaseapp.com",
-    databaseURL: "https://dummyzon-c93d1.firebaseio.com",
-    projectId: "dummyzon-c93d1",
-    storageBucket: "dummyzon-c93d1.appspot.com",
-    messagingSenderId: "277684560360",
-    appId: "1:277684560360:web:545d923d64f22ce5f3445d",
-    measurementId: "G-N8324QTYX8"
+    apiKey: "AIzaSyD960WbLIAeHMXrjYCv7C0t8wP97nYrrt8",
+    authDomain: "dummyzon-cb839.firebaseapp.com",
+    databaseURL: "https://dummyzon-cb839.firebaseio.com",
+    projectId: "dummyzon-cb839",
+    storageBucket: "dummyzon-cb839.appspot.com",
+    messagingSenderId: "490249574533",
+    appId: "1:490249574533:web:e0d865eedaca34fdc6f1ec",
+    measurementId: "G-5J70QLL2HL"
 })
 
 const db = firebaseApp.firestore();
